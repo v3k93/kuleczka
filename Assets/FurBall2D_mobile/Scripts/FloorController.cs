920f85d0d74052786b0f3cdc9d586b8baabf3678
Piotr Miron <piotrekmiron@gmail.com>
2018-12-02 21:35:31 +0100

    refresh

--- Assets/FurBall2D_mobile/Scripts/FloorController.cs
+++ Assets/FurBall2D_mobile/Scripts/FloorController.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class FloorController : MonoBehaviour {
 
+    public float lifeTime = 2;
+    private float time = 0;
+
 	// Use this for initialization
 	void Start () {
-		
+        
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        time += Time.deltaTime;
+		if (time > lifeTime)
+        {
+            Destroy(this);
+        }
 	}
 }
--- Assets/FurBall2D_mobile/Scripts/ObjectGenerator.cs
+++ Assets/FurBall2D_mobile/Scripts/ObjectGenerator.cs
@@ -9,15 +9,19 @@ public class ObjectGenerator : MonoBehaviour {
     public GameObject player;
     public float xLimit = 5;
     public float yLimit = 5;
+    private float lastY = 0;
 
     public float spawnFrequency = 2;
 
+    private float removeTime;
     private float time;
     private float nextSpawn;
+    private List<GameObject> initializedObjectsList = new List<GameObject>();
 
 	// Use this for initialization
 	void Start () {
         time = 0;
+        removeTime = -6;
         nextSpawn = 1 / spawnFrequency;
 	}
 	
@@ -26,6 +30,7 @@ public class ObjectGenerator : MonoBehaviour {
         float delta = Time.deltaTime;
 
         time += delta;
+        removeTime += delta;
 
         if (time > nextSpawn)
         {
@@ -33,20 +38,34 @@ public class ObjectGenerator : MonoBehaviour {
             Spawn();
 
         }
+
+        if (removeTime > nextSpawn)
+        {
+            removeTime = 0;
+            Debug.Log("REM1");
+            Remove();
+        }
+        
 	}
 
     void Spawn()
     {
         Vector3 playerPosition = player.transform.position;
+        float yBase = playerPosition.y > lastY ? playerPosition.y : lastY;
         float x = playerPosition.x + Random.Range(xLimit*-1, xLimit);
-        float y = playerPosition.y + yLimit;
+        float y = yBase + Random.Range(1, yLimit);
+        lastY = y;
         Debug.Log("player x " + x);
         Debug.Log("player y " + y);
-        Instantiate(ObjectToSpawn, new Vector3(x, y, 0), Quaternion.identity);
+        initializedObjectsList.Add(Instantiate(ObjectToSpawn, new Vector3(x, y, 0), Quaternion.identity));
+        
+    }
 
-
-
-
-
+    void Remove()
+    {
+        Debug.Log("REM2");
+        Destroy(initializedObjectsList[0]);
+        initializedObjectsList.RemoveAt(0);
+        Debug.Log("REM3");
     }
 }
